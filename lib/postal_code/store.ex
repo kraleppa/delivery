d5c74406a96ce4d48f7e47bbde9cd6c67e012e1d
krzysztof <misiek.nalepa@wp.pl>
2020-07-06 14:08:15 +0200

    basic store gen_server done

---
+++ lib/postal_code/store.ex
@@ -0,0 +1,21 @@
+defmodule PostalCode.Store do
+  use GenServer
+  alias PostalCode.DataParser
+
+  def start_link do
+    GenServer.start_link(__MODULE__, %{}, name: :postal_code_store)
+  end
+
+  def init(_) do
+    {:ok, DataParser.parse_data}
+  end
+
+  def get_coords(postal_code) do
+    GenServer.call(:postal_code_store, {:get_coords, postal_code})
+  end
+
+  def handle_call({:get_coords, postal_code}, _from, data) do
+    {:reply, Map.get(data, postal_code), data}
+  end
+
+end
---
+++ test/postal_code/store_test.exs
@@ -0,0 +1,12 @@
+defmodule PostalCode.StoreTest do
+  use ExUnit.Case
+  alias PostalCode.Store
+  doctest Delivery
+
+  test "get_coords" do
+    Store.start_link()
+    {lat, long} = Store.get_coords("32189")
+    assert {lat, long} == {29.55014, -81.647738}
+  end
+
+end
