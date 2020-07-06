2d8373304511aca2ebe295f045883b5a655af5a2
krzysztof <misiek.nalepa@wp.pl>
2020-07-06 13:54:56 +0200

    DataParser fix

--- lib/postal_code/data_parser.ex
+++ lib/postal_code/data_parser.ex
@@ -4,13 +4,20 @@ defmodule PostalCode.DataParser do
   def parse_data do
     [_ | data] = File.read!(@data_file) |> String.split("\n")
     data
-    |> Enum.map(fn (row) -> String.split(row, "\t") end)
-    |> Enum.filter(fn (row) -> length(row) == 7 end)
-    |> Enum.map(fn ([postal_code, _, _, _, _, lat, long]) -> [postal_code, lat, long] end)
-    |> Enum.map(fn ([postal_code, lat, long]) ->
-      float_lat = String.replace(lat, " ", "") |> String.to_float
-      float_long = String.replace(long, " ", "") |> String.to_float
-      {postal_code, {float_lat, float_long}} end)
+    |> Stream.map(&(String.split(&1, "\t")))
+    |> Stream.filter(&(length(&1) == 7))
+    |> Stream.map(&(column_parse(&1)))
+    |> Stream.map(&(coords_parse(&1)))
     |> Enum.into(%{})
+  end
+
+  defp column_parse [postal_code, _, _, _, _, lat, long] do
+    [postal_code, lat, long]
+  end
+
+  defp coords_parse [postal_code, lat, long] do
+    float_lat = String.replace(lat, " ", "") |> String.to_float
+    float_long = String.replace(long, " ", "") |> String.to_float
+    {postal_code, {float_lat, float_long}}
   end
 end
