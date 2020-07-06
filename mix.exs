884b322fbad22c807371a663fd322ddeef722fbc
krzysztof <misiek.nalepa@wp.pl>
2020-07-06 14:30:08 +0200

    application added

--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule Delivery.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {Delivery, []},
       extra_applications: [:logger]
     ]
   end
--- test/postal_code/store_test.exs
+++ test/postal_code/store_test.exs
@@ -4,7 +4,6 @@ defmodule Delivery.PostalCode.StoreTest do
   doctest Delivery
 
   test "get_coords" do
-    Store.start_link()
     {lat, long} = Store.get_coords("32189")
     assert {lat, long} == {29.55014, -81.647738}
   end
